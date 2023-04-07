7769f45fa42fe916545050bf4e533b2288a968dd
Luna Seifert <thepuddingdeity@gmail.com>
2023-04-07 14:38:05 -0500

    commit tree a369e9a55ab977f93a13b827da491d847661a078

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator name_arr
+    name_arr.map do |person|
+        badge_maker(person)
+    end
+end
+
+def assign_rooms name_arr
+    name_arr.map.with_index(1) do |person, index|
+        "Hello, #{person}! You'll be assigned to room #{index}!"
+    end
+end
+
+def printer name_arr
+    batch_badge_creator(name_arr).each {|person| puts person}
+    assign_rooms(name_arr).each {|person| puts person}
+end
